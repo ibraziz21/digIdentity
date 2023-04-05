b2163349c6e35a8cf7a396d12a19b7a13b8cc2a1
Ibrahim Aziz <ibraziz21@gmail.com>
2023-04-05 12:25:46 +0300

    Update Identity

--- contracts/Identity.sol
+++ contracts/Identity.sol
@@ -6,14 +6,17 @@ contract Identity {
             -Their Address
             -Proof of ID (password)
 */
+    address private owner;
+
     struct UserDetails {
         bytes32 proof;
         bool exists;
     }
 
     mapping (address => UserDetails) private identities;
+    mapping (address => bool) private hasAccess;
     constructor() {
-        
+        owner = msg.sender;
     }
     // Identity Creation
     //in its bare minimum, creation of identity requires a user address (unique Id) and some sort of proof
@@ -23,12 +26,28 @@ contract Identity {
         identities[msg.sender] = UserDetails(_proof, true);
     }
     //to verify identity, the msg.sender has to input the same proof as stored
-    function verifyIdentity(bytes32 _proof) public view returns (bool) {
-        if(identities[msg.sender].proof != _proof) revert UnableToVerify();
+    function verifyIdentity(address _address,bytes32 _proof) public view returns (bool) {
+        if(!identities(_address).exists) revert("Does Not Exist");
+        if(identities[_address].proof != _proof) revert UnableToVerify();
         return true;
     }
 
+//Only the msg.sender can delete their own identity
+    function deleteIdentity(bytes32 _proof) external {
+        require(identities[msg.sender].exists, "Identity does not exist");
+        if(identities[msg.sender].proof != _proof) revert("Unauthorized")
+        delete identities[msg.sender];
+    }
+
     //Access control using the identity
+    //Say, Our project has 2 other smart contracts. For a user to have access
+    // This can be used as a modifier in the other smart contracts
     
+    function gainAccess(bytes32 _proof) external {
+        if(verifyIdentity(msg.sender, _proof)){
+            hasAccess[msg.sender];
+        }
+    }
+
 
 }
